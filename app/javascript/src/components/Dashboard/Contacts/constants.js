cdbbbb0a8c58ad11839cfb873470fcbb7c0b4700
Alan Shaji <alanthomasshaji079@gmail.com>
2022-09-27 12:44:33 +0530

    Created new note UI and delete contact UI

--- app/javascript/src/components/Dashboard/Contacts/constants.js
+++ app/javascript/src/components/Dashboard/Contacts/constants.js
@@ -1,9 +1,19 @@
+import * as yup from "yup";
+
+export const CONTACTS_FORM_INITIAL_FORM_VALUES = {
+  firstName: "",
+  lastName: "",
+  role: null,
+  email: "",
+};
+
 export const TABLE_ROW_DATA = [
   {
     name: "Ronald Richards",
     role: "Owner",
     email: "alber@borer.com",
     createdAt: "Feb 8, 2021",
+    profile: "",
   },
   {
     name: "Jacob Jones",
@@ -17,6 +27,7 @@ export const TABLE_ROW_DATA = [
     role: "Owner",
     email: "alber@borer.com",
     createdAt: "Feb 8, 2021",
+    profile: "",
   },
   {
     name: "Jacob Jones",
@@ -25,4 +36,35 @@ export const TABLE_ROW_DATA = [
     createdAt: "Feb 9, 2021",
     profile: "https://i.pravatar.cc/300",
   },
+  {
+    name: "Ronald Richards",
+    role: "Owner",
+    email: "alber@borer.com",
+    createdAt: "Feb 8, 2021",
+    profile: "",
+  },
 ];
+
+export const ROLES = [
+  {
+    label: "Admin",
+    value: "admin",
+  },
+  {
+    label: "Non-Admin",
+    value: "non-admin",
+  },
+];
+
+export const CONTACTS_FORM_VALIDATION_SCHEMA = yup.object().shape({
+  firstName: yup.string().required("Enter First Name"),
+  lastName: yup.string().required("Enter Last Name"),
+  email: yup.string().email("Invalid e-mail format").required("Email required"),
+  role: yup
+    .object({
+      label: yup.string().oneOf(ROLES.map(role => role.label)),
+      value: yup.string().oneOf(ROLES.map(role => role.value)),
+    })
+    .nullable()
+    .required("Select a role"),
+});
